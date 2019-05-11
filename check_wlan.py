8fc584b835fe9464c45b46b96e03b74dd3952e7c
somsakwp8@outlook.com <23!?github>
2019-05-12 00:03:07 +0700

    delete unused files

--- check_wlan.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-ps = subprocess.Popen(['iwconfig'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-try:
-    output = subprocess.check_output(('grep', 'ESSID'), stdin=ps.stdout)
-    print(output)
-
-except subprocess.CalledProcessError:
-    # grep did not match any lines
-    print("No wireless networks connected")
--- test_get_ip.py
+++
@@ -1,8 +0,0 @@
-import json
-
-x = '{ "name":"John", "age":30, "city":"New York"}'
-y = json.loads(x)
-print(type(y))
-
-# the result is a Python dictionary:
-print(y["age"])
